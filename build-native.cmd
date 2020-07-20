809e5430797681debb1803e3dec824c09702bda2
David Maas <contact@pathogenstudios.com>
2020-07-20 16:02:55 -0500

    Changed LLVM from building release+symbols to just release.
    Having symbols is not super necessary and causes the build folder to take 5x as much space and the resulting binaries to be ~2x as large.
    We don't have a great way to distribute the symbols in a package either since NuGet symbol servers don't work with full PDBs.
    I may experiment with distributing a fatter ClangSharp.Pathogen.Runtime-Symbols package in the future, but for now I won't bother.

--- build-native.cmd
+++ build-native.cmd
@@ -20,7 +20,7 @@ if not exist build\build.ninja (
     cmake -G "Ninja" -S external/llvm-project/llvm/ -B build ^
         -DCMAKE_C_COMPILER=cl ^
         -DCMAKE_CXX_COMPILER=cl ^
-        -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
+        -DCMAKE_BUILD_TYPE=Release ^
         -DLLVM_ENABLE_PROJECTS=clang ^
         -DLLVM_INCLUDE_TESTS=off ^
         -DLLVM_INCLUDE_BENCHMARKS=off ^
