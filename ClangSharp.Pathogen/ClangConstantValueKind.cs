014a96acabea5a047dacb5d13a1345f96c3d8c6f
David Maas <contact@pathogenstudios.com>
2020-10-17 06:08:29 -0500

    Added ClangConstantValueKind. Added documentation to PathogenConstantValueInfo.

---
+++ ClangSharp.Pathogen/ClangConstantValueKind.cs
@@ -0,0 +1,25 @@
+﻿namespace ClangSharp.Pathogen
+{
+    /// <remarks>
+    /// This enum corresponds to <c>APValue::ValueKind</c>
+    /// 
+    /// Unlike most Clang enums, this one is not duplicated+verified in <c>PathogenExtensions.cpp</c>. As such, this enum is primarily intended for diagnostic use only.
+    /// </remarks>
+    public enum ClangConstantValueKind : int
+    {
+        None,
+        Indeterminate,
+        Int,
+        Float,
+        FixedPoint,
+        ComplexInt,
+        ComplexFloat,
+        LValue,
+        Vector,
+        Array,
+        Struct,
+        Union,
+        MemberPointer,
+        AddrLabelDiff
+    }
+}
--- ClangSharp.Pathogen/PathogenConstantValueInfo.cs
+++ ClangSharp.Pathogen/PathogenConstantValueInfo.cs
@@ -8,7 +8,21 @@ namespace ClangSharp.Pathogen
         public bool HasSideEffects;
         public bool HasUndefinedBehavior;
         public PathogenConstantValueKind Kind;
+        /// <summary>The subkind of the constant, see remarks for details.</summary>
+        /// <remarks>
+        /// If <see cref="Kind"/> is <see cref="PathogenConstantValueKind.UnsignedInteger"/>, <see cref="PathogenConstantValueKind.SignedInteger"/>, or <see cref="PathogenConstantValueKind.FloatingPoint"/>: This is the size of the value in bits.
+        /// If <see cref="Kind"/> is <see cref="PathogenConstantValueKind.String"/>, this is one of <see cref="PathogenStringConstantKind"/>.
+        /// If <see cref="Kind"/> is <see cref="PathogenConstantValueKind.Unknown"/>, this is one of <see cref="ClangConstantValueKind"/>.
+        /// </remarks>
         public int SubKind;
+        /// <summary>The value of the constant, see remarks for details.</summary>
+        /// <remarks>
+        /// If <see cref="Kind"/> is <see cref="PathogenConstantValueKind.NullPointer"/> or <see cref="PathogenConstantValueKind.Unknown"/>, this is 0.
+        /// If <see cref="Kind"/> is <see cref="PathogenConstantValueKind.UnsignedInteger"/> this is a zero-extended integer.
+        /// If <see cref="Kind"/> is <see cref="PathogenConstantValueKind.SignedInteger"/> this is a sign-extended integer.
+        /// If <see cref="Kind"/> is <see cref="PathogenConstantValueKind.FloatingPoint"/> this is the floating point value as bits and any unused bits are 0.
+        /// If <see cref="Kind"/> is <see cref="PathogenConstantValueKind.String"/> this is a pointer to a <see cref="PathogenConstantString"/> representing the string.
+        /// </remarks>
         public ulong Value;
     }
 }
