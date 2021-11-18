cdfadc8745d9b4767d64d8f0deb9d75b621815a5
David Maas <contact@pathogenstudios.com>
2021-11-18 07:43:42 -0600

    [CI] Updated sccache, added downloading of ARM64 binaries.

--- .github/workflows/install-sccache.py
+++ .github/workflows/install-sccache.py
@@ -2,6 +2,7 @@
 import hashlib
 import io
 import os
+import platform
 import stat
 import sys
 import tarfile
@@ -10,20 +11,29 @@ import urllib.request
 import gha
 
 # Configuration
-url_base = "https://github.com/mozilla/sccache/releases/download/0.2.12/"
+version = "v0.2.15"
+url_base = f"https://github.com/mozilla/sccache/releases/download/{version}/"
 
-platform = sys.platform
-if platform == 'win32':
-    expected_hash = "fd05e91c59b9497d4ebae311b47a982f2a6eb942dca3c9c314cc1fb36f8bc64d"
-    download_file_name = "sccache-0.2.12-x86_64-pc-windows-msvc"
+platform = f"{platform.system()}-{platform.machine()}"
+if platform == 'Windows-AMD64':
+    expected_hash = "3dfecdbb85561c55e899d3ad039c671f806d283c49da0721c2ef5c1310d87965"
+    download_file_name = f"sccache-{version}-x86_64-pc-windows-msvc"
     binary_name = "sccache.exe"
-elif platform == 'linux':
-    expected_hash = "26fd04c1273952cc2a0f359a71c8a1857137f0ee3634058b3f4a63b69fc8eb7f"
-    download_file_name = "sccache-0.2.12-x86_64-unknown-linux-musl"
+elif platform == 'Linux-x86_64':
+    expected_hash = "e5d03a9aa3b9fac7e490391bbe22d4f42c840d31ef9eaf127a03101930cbb7ca"
+    download_file_name = f"sccache-{version}-x86_64-unknown-linux-musl"
     binary_name = "sccache"
-elif platform == 'darwin':
-    expected_hash = "4945d295fa045dce9bf669d1313e4af4a504e74b08b61c1678902feb810c9ab7"
-    download_file_name = "sccache-0.2.12-x86_64-apple-darwin"
+elif platform == 'Linux-aarch64':
+    expected_hash = "90d91d21a767e3f558196dbd52395f6475c08de5c4951a4c8049575fa6894489"
+    download_file_name = f"sccache-{version}-aarch64-unknown-linux-musl"
+    binary_name = "sccache"
+elif platform == 'Darwin-x86_64':
+    expected_hash = "908e939ea3513b52af03878753a58e7c09898991905b1ae3c137bb8f10fa1be2"
+    download_file_name = f"sccache-{version}-x86_64-apple-darwin"
+    binary_name = "sccache"
+elif platform == 'Darwin-arm64':
+    expected_hash = "4120626b3a13b8e615e995b926db4166dc2b34274908b8f159ca65be4928b32a"
+    download_file_name = f"sccache-{version}-aarch64-apple-darwin"
     binary_name = "sccache"
 else:
     assert(False), f"Unknown platform '{platform}'"
